430a35d78095a6a3685230b93e2df9a6093162e2
tajreee <tajrianhaar@gmail.com>
2025-07-07 21:19:55 +0700

    feat: enhance token validation logging in useUserServer hook

--- src/hooks/useUserServer.ts
+++ src/hooks/useUserServer.ts
@@ -13,11 +13,14 @@ export default async function useUserServer(): Promise<User | undefined> {
   }
 
   try {
+    console.log('Validating token on server side...')
     const response = await customFetch<ValidateTokenResponse>(
       '/auth/me',
       { isAuthorized: true },
       cookies
     )
+
+    console.log('Token validation response:', response)
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { status, success, message, data } = response
@@ -25,7 +28,7 @@ export default async function useUserServer(): Promise<User | undefined> {
     if (status !== 200) {
       return undefined
     }
-
+    
     return data.user
   } catch (error) {
     // eslint-disable-next-line no-console
