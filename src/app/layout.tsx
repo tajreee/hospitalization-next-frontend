c55b13135acec146a996e6832b2c5fbb447b74ac
tajreee <tajrianhaar@gmail.com>
2025-07-08 22:55:43 +0700

    feat: move Toaster component inside AuthContextProvider for better context management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
       >
         <AuthContextProvider>
           {children}
+          <Toaster position="bottom-right" />
         </AuthContextProvider>
       </body>
     </html>
